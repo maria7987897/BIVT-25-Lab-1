486ea06943765099a1451d7d87cda0fea4dd5a47
maria7987897 <denimd240@mail.ru>
2025-09-22 22:41:43 +0300

    Update White.cs

--- Lab1/White.cs
+++ Lab1/White.cs
@@ -84,6 +84,18 @@
         {
             double answer = 0;
             // code here
+            double d = -1.1;
+            double f = -0.6;
+            if (Math.Abs(f) > Math.Abs(d))
+            {
+                Console.WriteLine(Math.Abs(d)); // возвращаем значение меньшего по модулю числа
+            }
+            else if (Math.Abs(d) > Math.Abs(f))
+            { 
+                Console.WriteLine(f); // возвращаем значение меньшего по модулю числа
+            }
+            else
+                Console.WriteLine(f); // числа по модулю равны => можно вывести любое из них, оно и будет наименьшим
             // end
             return answer;
         }
@@ -117,6 +129,17 @@
         {
             bool answer = false;
             // code here
+            double x = -1.1;
+            double y = -0.6;
+            double r = 1.5;
+            if (Math.Abs(x * x + y * y - r * r) <= Math.Pow(10, -4 ))
+            {
+                Console.WriteLine("yes"); 
+            }
+            else
+            { 
+                Console.WriteLine("no"); 
+            }
             // end
             return answer;
         }
@@ -134,6 +157,24 @@
         {
             bool answer = false;
             // code here
+            int n = 15;
+            int s = n * n;
+            bool answer = false;
+            if ((s - n) > (2 * n))
+            {
+                if (n % 2 == 0)
+                {
+                    answer = true;
+                }
+                else
+                {
+                    // Здесь ничего не делается = return answer
+                }
+            }
+            else
+            {
+                answer = true;
+            }
             // end
             return answer;
         }
@@ -151,11 +192,27 @@
         {
             bool answer = false;
             // code here
+            // произвольные текстовые значения: как пример
+            int L = 28; // Длина окружности острова
+            int T = 4; // Количество деревьев
+            int M = 2; // Количество гор
+
+            // 3 часа * 10 миль/ч = 30 миль(длина окружности по усл не более 30 миль)
+            // по условию четное кол-во гор и не менее 5 ориентиров, включая деревья
+            if (L <= 30 && T + M>= 5 && M % 2 == 0)
+            {
+                Console.WriteLine("Стоит причалить к этому острову. Тут клад");
+            }
+            else
+            {
+                Console.WriteLine("Не стоит причаливать к этому острову. Клад не тут");
+            }
             // end
             return answer;
         }
     }
 }
+
 
 
 
